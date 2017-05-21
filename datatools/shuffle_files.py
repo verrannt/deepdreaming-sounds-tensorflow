29980fab30a603e5ca07630f80591ab5fb2d4eea
verrannt <no@no.org>
2017-05-21 22:48:31 +0200

    several changes

---
+++ datatools/shuffle_files.py
@@ -0,0 +1,56 @@
+import os
+import random
+import sys
+
+if len(sys.argv) < 5:
+	print("Please use as 'python3 shuffle_files.py path training_samples validation_samples'")
+	exit(1)
+
+mode = sys.argv[1]
+p = sys.argv[2]
+t = sys.argv[3]
+v = sys.argv[4]
+
+#shuffle assigns TRAIN VALIDATE TEST flags to a specified
+#number of files randomly
+#train, validate specify how many files should be in each dataset
+#the rest will be assigned to testing
+def shuffle(path, train, validate):
+	
+	files = os.listdir(path)
+	random.shuffle(files)
+	print(files)
+	
+	if (train+validate+validate > len(files)):
+		print("There are not enough files for the parameters!")
+		exit(1) 
+	
+	training_set = files[:train]
+	validation_set = files[train:train + validate]
+	testing_set = files[validate+train:]
+
+	for file in training_set:
+		os.rename(os.path.join(path,file),os.path.join(path,"TRAIN-"+file))
+	for file in validation_set:
+		os.rename(os.path.join(path,file),os.path.join(path,"VALIDATE-"+file))
+	for file in testing_set:
+		os.rename(os.path.join(path,file),os.path.join(path,"TEST-"+file))	
+
+
+#removes flags set by shuffle()
+def unshuffle(path):
+
+	files = os.listdir(path)
+	
+	for file in files:
+
+		if any(a ==  str(file).split("-")[0] for a in ["TRAIN", "TEST", "VALIDATE"] ):
+			os.rename(os.path.join(path,file) , os.path.join(path , str(file).split("-",1)[1]))		
+
+
+if mode == "shuffle":
+	shuffle(p,int(t),int(v))
+elif mode == "unshuffle":
+	unshuffle(p)
+else:
+	print("Please use as 'python3 shuffle_files.py path training_samples validation_samples'")		
---
+++ utilities_preversion.py
@@ -0,0 +1,62 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import matplotlib.mlab
+from os import listdir
+from os.path import isfile, join, isdir
+from scipy.io import wavfile
+
+class Util():
+
+    def generate_batch(self, batch_size, path, sample_length):
+
+        ### TODO AT THE MOMENT WE ARE ALWAYS USING THE COMPLETE SAMPLE LENGTH
+
+        samples_per_class = batch_size // 9
+        assert samples_per_class <= 120, "Too many samples per class, max 120. \
+            Reduce batch size!"
+
+        train_size, val_size, test_size = [int(samples_per_class*i) for i in [0.6, 0.2, 0.2]]
+        trainX, valX, testX = [[] for _ in range(3)]
+        self.classes = [f for f in listdir(path) if isdir(join(path,f))]
+
+        for self.class_name in self.classes:
+            for _ in range(train_size):
+                X = get_random_spec(self,"TRAIN")
+                trainX.append([self.class_name, X])
+            for _ in range(val_size):
+                X = get_random_spec(self,"VALIDATE")
+                valX.append([self.class_name, X])
+            for _ in range(test_size):
+                X = get_random_spec(self,"TEST")
+                trainX.append([self.class_name, X])
+
+        np.random.shuffle(trainX)
+        np.random.shuffle(valX)
+        np.random.shuffle(testX)
+
+        trainY = make_one_hot(trainX)
+        valY = make_one_hot(valX)
+        testY = make_one_hot(testY)
+
+        return trainX, trainY, valX, valY, testX, testY
+
+    def get_random_spec(self, set):
+        all_files = listdir(join(path,self.class_name))
+        suitable_files = [f for f in all_files if f.split("-")[0] == set]
+        random_file = random.choice(suitable_files)
+        wav_file = wavfile.read(join(path+self.class_name, random_file))
+        wav_file_n = wav_data/np.max(np.abs(wav_data),axis=0)
+        spec = matplotlib.mlab.specgram(wav_file_n)[0]
+        return spec
+
+    def make_one_hot(self, samples):
+        labels = samples[:,0]
+        indexes = [self.classes.index(f) for f in labels]
+        one_hot = np.zeros([len(samples),9])
+        for i, index_value in enumerate(indexes):
+            one_hot[i][index_value] = 1
+        return one_hot
+
+U = Util()
+trainX, trainY, valX, valY, testX, testY = U.generate_batch(batch_size=100,path="../TrainingData/UrbanSound8K_modified_v1/audio/",sample_length=1)
